406851630d93e292be015c486c49e3d5cbacaa02
adimonsa <adimonsa93@gamil.com>
2022-11-23 13:35:25 +0200

    helloe java file changes

--- AdiMonsa.java
+++ AdiMonsa.java
@@ -1,7 +1,7 @@
 public class AdiMonsa {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Adi Monsa" + i);
+            System.out.println("Adi Monsa hiiii" + i);
         }
     }
 }
